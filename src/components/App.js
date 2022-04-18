af26d2837897443127e38c6749d539f8ac8937a3
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-18 13:21:35 +0700

    Headers, about, teachers, courses routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,20 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 // App components
 
+import Header from "./Header";
 import Home from "./Home";
+import About from "./About";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
     </div>
   </BrowserRouter>
 );
