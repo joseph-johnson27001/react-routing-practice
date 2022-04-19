08b68be89615b675de3fe0f8a31640d422ca5d4a
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-19 10:16:31 +0700

    URL parameters for teachers and courses

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Featured = () => {
-
+const Featured = ({ match }) => {
+  let name = `${match.params.fname} ${match.params.lname}`;
+  let topic = match.params.topic;
   return (
     <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong></strong>, a teacher who loves teaching courses about <strong></strong>!</p>
+      <h2>{name.toUpperCase()}: </h2>
+      <p>
+        Introducing <strong>{name.toUpperCase()}</strong>, a teacher who loves
+        teaching courses about <strong>{topic.toUpperCase()}</strong>!
+      </p>
     </div>
   );
-}
+};
 
 export default Featured;
